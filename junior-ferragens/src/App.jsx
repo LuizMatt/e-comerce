a46955ff435f5252d8307469983369b67d8f6c0d
Luiz Matheus <96705426+LuizMatt@users.noreply.github.com>
2025-06-27 17:25:04 -0300

    feat: atualização da interface do Card.jsx

--- junior-ferragens/src/App.jsx
+++ junior-ferragens/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
--- junior-ferragens/src/components/card/Card.jsx
+++ junior-ferragens/src/components/card/Card.jsx
@@ -1,17 +1,25 @@
-  import React from "react";
-  import "./Card.css"; 
+import React from "react";
+import "./Card.css";
 
-  const Card = ({ title, description, imageUrl }) => {
-    return (
-      <div className="card">
-        <img src={imageUrl} alt={title} className="card-image" />
-        <div className="card-content">
-          <h3 className="card-title">{title}</h3>
-          <p className="card-description">{description}</p>
-          <button className="card-button"> Adicionar ao carrinho</button>
+const Card = ({ title, description, imageUrl, price }) => {
+  return (
+    <div className="card">
+      <img
+        src={`http://localhost:3000${imageUrl}`}
+        alt={title}
+        className="card-image"
+      />
+      <div className="card-content">
+        <h3 className="card-title">{title}</h3>
+        <p className="card-description">{description}</p>
+        <p className="card-price">R$ {parseFloat(price).toFixed(2)}</p>
+        <div className="card-buttons">
+          <button className="card-button">Adicionar ao carrinho</button>
+          <button className="card-outline">Fazer orçamento</button>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Card;
+export default Card;
