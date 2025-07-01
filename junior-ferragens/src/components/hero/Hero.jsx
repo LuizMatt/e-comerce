1bb350aa1048d14015c926b43c18c9803f09a260
Luiz Matheus <96705426+LuizMatt@users.noreply.github.com>
2025-07-01 16:10:47 -0300

    feat(Hero): adicionando nova section ao Hero

--- junior-ferragens/src/components/hero/Hero.jsx
+++ junior-ferragens/src/components/hero/Hero.jsx
@@ -4,6 +4,9 @@ import supportIcon from "../../assets/support.svg";
 import truckIcon from "../../assets/truck.svg";
 import percentIcon from "../../assets/percent.svg";
 import FeaturedProducts from "../featuredProducts/FeaturedProducts";
+import lupa from "../../assets/lupa.svg";
+import carrinho from "../../assets/carrinho.svg";
+import speedTruck from "../../assets/speedTruck.svg";
 
 const Hero = () => {
   return (
@@ -41,6 +44,45 @@ const Hero = () => {
 
       <section className="featured-container">
         <FeaturedProducts />
+      </section>
+      <section className="como-funciona">
+        <div className="como-funciona-content">
+          <h2>Como funciona?</h2>
+          <p>
+            Navegue por nossa seleção de ferramentas, escolha as que mais
+            combinam com suas necessidades e aproveite nossos preços
+            competitivos. Compre online e receba no conforto da sua casa.
+          </p>
+        </div>
+
+        <div className="cards-container">
+          <div className="info-box">
+            <img src={lupa} alt="Escolha o produto" />
+            <h3>Escolha o produto</h3>
+            <p>
+              Navegue pelo nosso catálogo e selecione o produto ideal para sua
+              necessidade
+            </p>
+          </div>
+
+          <div className="info-box">
+            <img src={carrinho} alt="Adicione ao carrinho" />
+            <h3>Adicione ao carrinho</h3>
+            <p>
+              Adicione o produto ao carrinho e finalize sua compra de forma
+              rápida e segura
+            </p>
+          </div>
+
+          <div className="info-box">
+            <img src={speedTruck} alt="Receba o pedido" />
+            <h3>Receba o pedido</h3>
+            <p>
+              Receba seu pedido no conforto da sua casa com nossa entrega rápida
+              e eficiente
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
